a511b7f948cfde01fa091ba6f3dfd57546729a82
carolquiterio <carolquiterio@yahoo.com.br>
2023-11-13 10:27:41 -0300

    Add state view

--- AlimFoco/DayPlanView.swift
+++ AlimFoco/DayPlanView.swift
@@ -10,121 +10,148 @@ import SwiftUI
 struct DayPlanView: View {
     @EnvironmentObject private var model: Model
     @State var selectedDate = Date()
-    var MealItems: [MealItem] {        
+    var MealItems: [MealItem] {
         model.Mealitems
     }
-
+    
     var body: some View {
-        VStack (alignment: .leading){
-            DateSelectorView(dates: dates(for: Date()), selectedDate: $selectedDate)
-            
-            Spacer()
-            
-            List{
-                Section(header: Text("Refeições")) {
-                    DisclosureGroup("Café da Manhã") {
-                        VStack(alignment: .leading) {
-                            ForEach(MealItems, id: \.recordId){ mealItem in
-                                Text(mealItem.title)
-                            }.onDelete { indexSet in
-                                guard let index = indexSet.map({ $0 }).last else {
-                                return
+        NavigationView {
+            VStack (alignment: .center){
+                DateSelectorView(dates: dates(for: Date()), selectedDate: $selectedDate)
+                Spacer()
+                if(false) {
+                    VStack () {
+                        Spacer()
+                        ErrorState(
+                            image: "",
+                            title: "Ops! Está vazio.",
+                            description: "Não há refeições a serem exibidas para este dia.",
+                            buttonText: "Criar nova refeição",
+                            action: {
+                               // NavigationLink(destination: NewMealView())
+                            }
+                        )
+                        Spacer()
+                    }
+                } else {
+                    List{
+                        Section(header: Text("Refeições")) {
+                            DisclosureGroup("Café da Manhã") {
+                                VStack(alignment: .leading) {
+                                    ForEach(MealItems, id: \.recordId){ mealItem in
+                                        Text(mealItem.title)
+                                    }.onDelete { indexSet in
+                                        guard let index = indexSet.map({ $0 }).last else {
+                                            return
+                                        }
+                                        let MealItem = model.Mealitems[index]
+                                        Task {
+                                            do {
+                                                try await model.deleteItem(MealItemToBeDeleted: MealItem)
+                                            } catch {
+                                                print(error)
+                                            }
+                                        }
+                                    }
                                 }
-                                let MealItem = model.Mealitems[index]
-                                Task {
-                                    do {
-                                        try await model.deleteItem(MealItemToBeDeleted: MealItem)
-                                    } catch {
-                                        print(error)
+                            }
+                            
+                            DisclosureGroup("Colação") {
+                                ForEach(MealItems, id: \.recordId){ mealItem in
+                                    Text(mealItem.title)
+                                }.onDelete { indexSet in
+                                    guard let index = indexSet.map({ $0 }).last else {
+                                        return
+                                    }
+                                    let MealItem = model.Mealitems[index]
+                                    Task {
+                                        do {
+                                            try await model.deleteItem(MealItemToBeDeleted: MealItem)
+                                        } catch {
+                                            print(error)
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            DisclosureGroup("Almoçar") {
+                                ForEach(MealItems, id: \.recordId){ mealItem in
+                                    Text(mealItem.title)
+                                }.onDelete { indexSet in
+                                    guard let index = indexSet.map({ $0 }).last else {
+                                        return
+                                    }
+                                    let MealItem = model.Mealitems[index]
+                                    Task {
+                                        do {
+                                            try await model.deleteItem(MealItemToBeDeleted: MealItem)
+                                        } catch {
+                                            print(error)
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            DisclosureGroup("Lanche") {
+                                ForEach(MealItems, id: \.recordId){ mealItem in
+                                    Text(mealItem.title)
+                                }.onDelete { indexSet in
+                                    guard let index = indexSet.map({ $0 }).last else {
+                                        return
+                                    }
+                                    let MealItem = model.Mealitems[index]
+                                    Task {
+                                        do {
+                                            try await model.deleteItem(MealItemToBeDeleted: MealItem)
+                                        } catch {
+                                            print(error)
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            DisclosureGroup("Jantar") {
+                                ForEach(MealItems, id: \.recordId){ mealItem in
+                                    Text(mealItem.title)
+                                }.onDelete { indexSet in
+                                    guard let index = indexSet.map({ $0 }).last else {
+                                        return
+                                    }
+                                    let MealItem = model.Mealitems[index]
+                                    Task {
+                                        do {
+                                            try await model.deleteItem(MealItemToBeDeleted: MealItem)
+                                        } catch {
+                                            print(error)
+                                        }
                                     }
                                 }
                             }
                         }
+                    }.headerProminence(.increased)
+                        .frame(height: getHeight() / 18)
+                }
+                Spacer()
+            }.task {
+                do {
+                    try await model.populateMealItems()
+                } catch {
+                    VStack () {
+                        Spacer()
+                        ErrorState(
+                            image: "",
+                            title: "Ops! Algo deu errado.",
+                            description: "Parece que você está sem conexão.",
+                            buttonText: "Tente novamente",
+                            action: {}
+                        )
+                        Spacer()
                     }
-                    
-                    DisclosureGroup("Colação") {
-                        ForEach(MealItems, id: \.recordId){ mealItem in
-                            Text(mealItem.title)
-                        }.onDelete { indexSet in
-                            guard let index = indexSet.map({ $0 }).last else {
-                            return
-                            }
-                            let MealItem = model.Mealitems[index]
-                            Task {
-                                do {
-                                    try await model.deleteItem(MealItemToBeDeleted: MealItem)
-                                } catch {
-                                    print(error)
-                                }
-                            }
-                        }
-                    }
-                    
-                    DisclosureGroup("Almoçar") {
-                        ForEach(MealItems, id: \.recordId){ mealItem in
-                            Text(mealItem.title)
-                        }.onDelete { indexSet in
-                            guard let index = indexSet.map({ $0 }).last else {
-                            return
-                            }
-                            let MealItem = model.Mealitems[index]
-                            Task {
-                                do {
-                                    try await model.deleteItem(MealItemToBeDeleted: MealItem)
-                                } catch {
-                                    print(error)
-                                }
-                            }
-                        }
-                    }
-                    
-                    DisclosureGroup("Lanche") {
-                        ForEach(MealItems, id: \.recordId){ mealItem in
-                            Text(mealItem.title)
-                        }.onDelete { indexSet in
-                            guard let index = indexSet.map({ $0 }).last else {
-                            return
-                            }
-                            let MealItem = model.Mealitems[index]
-                            Task {
-                                do {
-                                    try await model.deleteItem(MealItemToBeDeleted: MealItem)
-                                } catch {
-                                    print(error)
-                                }
-                            }
-                        }
-                    }
-                    
-                    DisclosureGroup("Jantar") {
-                        ForEach(MealItems, id: \.recordId){ mealItem in
-                            Text(mealItem.title)
-                        }.onDelete { indexSet in
-                            guard let index = indexSet.map({ $0 }).last else {
-                            return
-                            }
-                            let MealItem = model.Mealitems[index]
-                            Task {
-                                do {
-                                    try await model.deleteItem(MealItemToBeDeleted: MealItem)
-                                } catch {
-                                    print(error)
-                                }
-                            }
-                        }
-                    }
-                }.headerProminence(.increased)
-                    .frame(height: getHeight() / 18)
+                    print(error)
+                }
             }
-            
-        }.task {
-            do {
-                try await model.populateMealItems()
-            } catch {
-                print(error)
-            }
+            .background(Color(.systemGroupedBackground))
         }
-        .background(Color(.systemGroupedBackground))
     }
     
     func dates(for startDate: Date) -> [Date] {
@@ -143,7 +170,3 @@ struct DayPlanView_Previews: PreviewProvider {
         DayPlanView().environmentObject(Model())
     }
 }
-
-
-
-//}
