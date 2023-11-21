a9a5cd27b8c84289cda212ef4413405b155f86ff
Gustavo Sacramento <gustavo.sacramento211@gmail.com>
2023-11-21 15:57:53 -0300

    add item filter according to json

---
+++ AlimFoco/FilterView.swift
@@ -0,0 +1,18 @@
+//
+//  FIlterView.swift
+//  AlimFoco
+//
+//  Created by Gustavo Sacramento on 21/11/23.
+//
+
+import SwiftUI
+
+struct FIlterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FIlterView()
+}
