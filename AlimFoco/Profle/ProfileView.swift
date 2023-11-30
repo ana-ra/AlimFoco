1afb8bede8760a51d0ba192f17e656b87297d2e5
carolquiterio <carolquiterio@yahoo.com.br>
2023-11-29 23:26:30 -0300

    Add profile view

--- AlimFoco/Profle/ProfileView.swift
+++ AlimFoco/Profle/ProfileView.swift
@@ -8,33 +8,93 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var isDeleteAccountSheetViewPresented = false
+    @State private var isLogoutViewSheetViewPresented = false
+    
     var body: some View {
-        NavigationStack {
-            List {
-                Section(header: Text("CONFIGURAÇÕES")) {
-                    Text("Nome: John Doe")
-                    Text("Idade: 30 anos")
-                    Text("Cidade: Exemploville")
-                }
-                
-                Section(header: Text("Configurações")) {
-                    NavigationLink(destination: Text("Detalhes da Conta")) {
-                        HStack {
-                            Text("Conta")
-                            VStack {
-                                Text("Conta")
-                                Text("Conta")
-
+        VStack {
+            NavigationStack {
+                List {
+                    Section(header: Text("Configurações")) {
+                        NavigationLink(destination: Text("Assinatura")) {
+                            HStack {
+                                Image(systemName: "bag")
+                                    .frame(width: 16, height: 16)
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 12)
+                                VStack (alignment: .leading, content: {
+                                    Text("Assinatura")
+                                        .font(.system(size: 16))
+                                    Text("Planos de assinatura do app")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                    
+                                })
                             }
+                            
                         }
-                        
+                        NavigationLink(destination: Text("Histórico")) {
+                            HStack {
+                                Image(systemName: "wallet.pass")
+                                    .frame(width: 16, height: 16)
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 12)
+                                VStack (alignment: .leading, content: {
+                                    Text("Histórico")
+                                        .font(.system(size: 16))
+                                    Text("Meu histórico de refeições")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                    
+                                })
+                            }
+                            
+                        }
+                        NavigationLink(destination: TotalMealsView()) {
+                            HStack {
+                                Image(systemName: "carrot")
+                                    .frame(width: 16, height: 16)
+                                    .foregroundColor(.black)
+                                    .padding(.trailing, 12)
+                                VStack (alignment: .leading, content: {
+                                    Text("Refeições")
+                                        .font(.system(size: 16))
+                                    Text("Meu inventário de refeições")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(.gray)
+                                    
+                                })
+                            }
+                            
+                        }
                     }
-                    NavigationLink(destination: Text("Preferências")) {
-                        Text("Preferências")
+                    
+                    Section(header: Text("Conta")) {
+                        Button(action: {
+                            isLogoutViewSheetViewPresented.toggle()
+                        }) {
+                            Text("Sair")
+                                .font(.system(size: 16))
+                                .foregroundColor(.errorRed)
+                        }
+                        Button(action: {
+                            isDeleteAccountSheetViewPresented.toggle()
+                        }) {
+                            Text("Deletar conta")
+                                .font(.system(size: 16))
+                                .foregroundColor(.errorRed)
+                        }
                     }
-                }
-            }.navigationTitle("Carolina Q")
-        }
+                }.navigationTitle("Carolina Q")
+                
+            }
+        }.sheet(isPresented: $isDeleteAccountSheetViewPresented, content: {
+            ProfileAccountSheetView(title: "Tem certeza de que deseja deletar sua conta?", secondaryButtonTitle: "Deletar conta").presentationDetents([.height(getHeight() / 3.5)])
+            
+        })
+        .sheet(isPresented: $isLogoutViewSheetViewPresented, content: {
+            ProfileAccountSheetView(title: "Tem certeza de que deseja sair do app?", secondaryButtonTitle: "Sair").presentationDetents([.height(getHeight() / 3.5)])
+        })
     }
 }
 
