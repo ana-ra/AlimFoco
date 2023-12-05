a13b1fe2d8c84eaa6f5cc290571ee188324ff575
Ana Laura Alves <aanalaura852@gmail.com>
2023-12-05 11:59:25 -0300

    adicao sheet refeicoes e discolapsavel dinamico

---
+++ AlimFoco/DayPlan/MealSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  MealSheetView.swift
+//  AlimFoco
+//
+//  Created by Ana Laura Alves Cordeiro on 05/12/23.
+//
+
+import SwiftUI
+
+struct MealSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealSheetView()
+}
