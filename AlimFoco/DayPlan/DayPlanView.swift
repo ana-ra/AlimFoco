1491b6f240c9e3495a5f23cb2aa006e47ae74164
lariokabayashi <l247220@dac.unicamp.br>
2023-12-06 09:51:01 -0300

    add register satisfaction

--- AlimFoco/DayPlan/DayPlanView.swift
+++ AlimFoco/DayPlan/DayPlanView.swift
@@ -12,11 +12,11 @@ struct DayPlanView: View {
     @Binding var hasLoggedIn: Bool
     @EnvironmentObject private var model: Model
     @EnvironmentObject private var modelMeal: ModelMeal
-    @EnvironmentObject private var modelMealType: ModelMealType
     @State var selectedMeal: String = ""
     @State var selectedDate = Date()
     @State var isNavigatingToNewMealView = false
     @State var isSatisfactionSheetPresented = false
+    @State var filteredMealsState: [Meal] = []
     var mealItems: [MealItem] {
         model.Mealitems
     }
@@ -45,28 +45,29 @@ struct DayPlanView: View {
                         Spacer()
                     }.padding(16)
                 } else {
-                    List {
+                    List{
                         Section(header: Text("Próximas Refeições")) {
                             ForEach(mealTypes.indices) { index in
                                 let filteredMeals = meals.filter { meal in
-                                    meal.mealType == mealTypes[index]
+                                    meal.mealType == mealTypes[index] && meal.registered == 0
                                 }
-                            
+                                
                                 if !filteredMeals.isEmpty {
                                     DisclosureGroup {
                                         CardScrollView(meals: filteredMeals)
                                             .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
-                                         Button(action: {
+                                        Button(action: {
+                                            filteredMealsState = filteredMeals
                                             selectedMeal = mealTypes[index]
                                             isSatisfactionSheetPresented.toggle()
                                         }) {
                                             HStack(alignment: .center, spacing: 4) {
                                                 Image(systemName: "note.text.badge.plus")
                                                     .foregroundColor(Color.white)
-
+                                                
                                                 Text("Registrar")
                                                     .foregroundColor(Color.white)
-                      
+                                                
                                             }
                                             .padding(.horizontal, 16)
                                             .frame(width: getWidth() / 2.8, height: getHeight() / 20)
@@ -75,7 +76,7 @@ struct DayPlanView: View {
                                         }
                                         .padding(.vertical, 8)
                                         .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
-
+                                        
                                     } label: {
                                         Text(mealTypes[index])
                                             .fontWeight(.semibold)
@@ -86,16 +87,52 @@ struct DayPlanView: View {
                             }
                             
                         }.listRowBackground(Color(red: 0.95, green: 0.95, blue: 0.97))
-                        .headerProminence(.increased)
-                        .background(Color(red: 0.95, green: 0.95, blue: 0.97))
-                         
+                            .headerProminence(.increased)
+                            .background(Color(red: 0.95, green: 0.95, blue: 0.97))
+                        
                         Section(header: Text("Registrado")) {
-                            
+                            ForEach(mealTypes.indices) { index in
+                                let filteredMeals = meals.filter { meal in
+                                    meal.mealType == mealTypes[index] && meal.registered == 1
+                                }
+                                
+                                if !filteredMeals.isEmpty {
+                                    DisclosureGroup {
+                                        CardScrollView(meals: filteredMeals)
+                                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
+                                        Button(action: {
+                                            filteredMealsState = filteredMeals
+                                            selectedMeal = mealTypes[index]
+                                            isSatisfactionSheetPresented.toggle()
+                                        }) {
+                                            HStack(alignment: .center, spacing: 4) {
+                                                Image(systemName: "note.text.badge.plus")
+                                                    .foregroundColor(Color.white)
+                                                
+                                                Text("Registrar")
+                                                    .foregroundColor(Color.white)
+                                                
+                                            }
+                                            .padding(.horizontal, 16)
+                                            .frame(width: getWidth() / 2.8, height: getHeight() / 20)
+                                            .background(Color(red: 0.05, green: 0.51, blue: 0.44))
+                                            .cornerRadius(14)
+                                        }
+                                        .padding(.vertical, 8)
+                                        .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 16))
+                                        
+                                    } label: {
+                                        Text(mealTypes[index])
+                                            .fontWeight(.semibold)
+                                    }
+                                    .listRowSeparator(.hidden)
+                                    .listRowInsets(EdgeInsets(top: 50, leading: 20, bottom: 20, trailing: 10))
+                                }
+                            }
                         }
                         .headerProminence(.increased)
                     }
                     .background(Color(red: 0.95, green: 0.95, blue: 0.97))
-                    
                 }
             }.background(Color(red: 0.95, green: 0.95, blue: 0.97))
             .onAppear {
@@ -103,7 +140,6 @@ struct DayPlanView: View {
                     if hasLoggedIn {
                         do {
                             try await loadMealData()
-                            print(modelMeal.Meals)
                         } catch {
                             errorView()
                             print(error)
@@ -140,8 +176,8 @@ struct DayPlanView: View {
             }
             .navigationTitle("Plano Alimentar")
             .sheet(isPresented: $isSatisfactionSheetPresented, content: {
-                RegisterSatisfactionSheetView(selectedDate: $selectedDate, meal: $selectedMeal).presentationDetents([.height(getHeight() / 3.5)])
-                    .tint(Color.informationGreen).environmentObject(ModelMealType())
+                RegisterSatisfactionSheetView(selectedMeal: $selectedMeal, filteredMeals: $filteredMealsState).presentationDetents([.height(getHeight())])
+                    .tint(Color.informationGreen).environmentObject(ModelMeal()).background(Color(red: 0.95, green: 0.95, blue: 0.97))
             })
         }
     }
@@ -175,11 +211,11 @@ struct DayPlanView: View {
     }
 }
                             
-struct DayPlanView_Previews: PreviewProvider {
-    static var previews: some View {
-        DayPlanView(
-            isPresentingOnboarding: .constant(true),
-            hasLoggedIn: .constant(false)
-        ).environmentObject(Model())
-    }
-}
+//struct DayPlanView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        DayPlanView(
+//            isPresentingOnboarding: .constant(true),
+//            hasLoggedIn: .constant(false)
+//        ).environmentObject(Model())
+//    }
+//}
